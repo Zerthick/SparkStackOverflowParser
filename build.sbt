5acf6193900c0d84265b1b75e9013abbccfc5c8c
Zerthick <zerthick@gmail.com>
2017-11-22 17:40:32 -0500

    Get assembly working

--- build.sbt
+++ build.sbt
@@ -8,4 +8,25 @@ libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.2.0"
 
 // https://mvnrepository.com/artifact/com.databricks/spark-xml_2.11
 libraryDependencies += "com.databricks" % "spark-xml_2.11" % "0.4.1"
-        
+
+assemblyMergeStrategy in assembly := {
+  case PathList("org","aopalliance", xs @ _*) => MergeStrategy.last
+  case PathList("javax", "inject", xs @ _*) => MergeStrategy.last
+  case PathList("javax", "servlet", xs @ _*) => MergeStrategy.last
+  case PathList("javax", "activation", xs @ _*) => MergeStrategy.last
+  case PathList("org", "apache", xs @ _*) => MergeStrategy.last
+  case PathList("com", "google", xs @ _*) => MergeStrategy.last
+  case PathList("com", "esotericsoftware", xs @ _*) => MergeStrategy.last
+  case PathList("com", "codahale", xs @ _*) => MergeStrategy.last
+  case PathList("com", "yammer", xs @ _*) => MergeStrategy.last
+  case "about.html" => MergeStrategy.rename
+  case "META-INF/ECLIPSEF.RSA" => MergeStrategy.last
+  case "META-INF/mailcap" => MergeStrategy.last
+  case "META-INF/mimetypes.default" => MergeStrategy.last
+  case "plugin.properties" => MergeStrategy.last
+  case "log4j.properties" => MergeStrategy.last
+  case "overview.html" => MergeStrategy.rename
+  case x =>
+    val oldStrategy = (assemblyMergeStrategy in assembly).value
+    oldStrategy(x)
+}
--- src/main/scala/SparkMain.scala
+++ src/main/scala/SparkMain.scala
@@ -1,12 +1,17 @@
 import org.apache.spark.sql.SparkSession
+import org.apache.log4j.Logger
+import org.apache.log4j.Level
 
 object SparkMain {
 
   def main(args: Array[String]) {
 
-    if (args.length != 2) {
+    Logger.getLogger("org").setLevel(Level.OFF)
+    Logger.getLogger("akka").setLevel(Level.OFF)
 
-      println("Correct usage: scala SparkMain <input.xml> <output.xml>")
+    if (args.length != 3) {
+
+      println("Correct usage: scala SparkMain <master> <input.xml> <output.xml>")
 
       return
     }
@@ -14,21 +19,23 @@ object SparkMain {
     val spark = SparkSession
       .builder()
       .appName("Word Count XML")
-      .master("local")
+      .master(args(0))
       .getOrCreate()
 
     import spark.implicits._
+
+    val startTime = System.currentTimeMillis()
 
     val inputFile = spark.read
       .format("com.databricks.spark.xml")
       .option("rootTag", "posts")
       .option("rowTag", "row")
-      .load(args(0))
+      .load(args(1))
 
     val cleanedPosts = inputFile.select("_ID", "_Body")
       .map(row => (row.getLong(0) , row.getString(1).toLowerCase.replaceAll("\\s+", " ").replaceAll("(<.*?>|['\"])", "")))
       .withColumnRenamed("_1", "id")
-      .withColumnRenamed("_2", "body")
+      .withColumnRenamed("_2", "body").cache()
 
     val ds = cleanedPosts.as[(Long, String)]
 
@@ -38,11 +45,11 @@ object SparkMain {
 
     val filteredWords = splitWords.map(row => (row.getLong(0), row.getString(1).trim)).filter(row => row._2.length > 2 && row._2.matches("[a-z]+.*?"))
       .withColumnRenamed("_1", "id")
-      .withColumnRenamed("_2", "word")
+      .withColumnRenamed("_2", "word").cache()
 
     val tf = filteredWords.select("word").map(row => (row.getString(0), 1)).groupBy("_1").sum("_2")
       .withColumnRenamed("_1", "wordtf")
-      .withColumnRenamed("sum(_2)", "ctf").as("tf")
+      .withColumnRenamed("sum(_2)", "ctf").as("tf").cache()
 
     val df = filteredWords.distinct().select("word").map(row => (row.getString(0), 1)).groupBy("_1").sum("_2")
       .withColumnRenamed("_1", "worddf")
@@ -63,6 +70,10 @@ object SparkMain {
       .format("com.databricks.spark.xml")
       .option("rootTag", "posts")
       .option("rowTag", "row")
-      .save(args(1))
+      .save(args(2))
+
+    val endTime = System.currentTimeMillis()
+
+    println("Total Runtime: " + (endTime - startTime))
   }
 }
